8192e3d41318a30086751659ceccfd1791eb90dd
CC <mowen@126.com>
2025-12-25 15:14:01 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
@@ -8,7 +11,8 @@ export default defineConfig({
     base: './',
     resolve: {
         alias: {
-            '@': resolve(__dirname, 'src')
+            // '@': resolve(__dirname, 'src')
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
     server: {
